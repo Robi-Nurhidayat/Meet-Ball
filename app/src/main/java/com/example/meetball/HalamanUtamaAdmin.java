a2b920a832ac1d3c37db4b9b796ef94196be0c61
Bagus Perdana <bagus.10118249@mahasiswa.unikom.ac.id>
2021-08-06 21:37:18 +0700

    Memperbaiki UI Admin

--- app/src/main/java/com/example/meetball/HalamanUtamaAdmin.java
+++ app/src/main/java/com/example/meetball/HalamanUtamaAdmin.java
@@ -6,6 +6,7 @@ import androidx.cardview.widget.CardView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.meetball.minuman.MinumanAdmin;
 import com.example.meetball.pemasukkan.PemasukkanAdmin;
@@ -15,13 +16,15 @@ import com.example.meetball.produk_baru.ProdukBaru;
 public class HalamanUtamaAdmin extends AppCompatActivity {
 
     CardView card_produk,card_pesanan_admin,card_pemasukkan,card_minuman;
-
+    Button logoutAdmin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_halaman_utama_admin);
 
+
+        logoutAdmin = findViewById(R.id.logoutAdmin);
         card_minuman = findViewById(R.id.card_view_minuman);
         card_pesanan_admin = findViewById(R.id.card_view_pesanan);
         card_produk = findViewById(R.id.card_produk);
@@ -56,6 +59,14 @@ public class HalamanUtamaAdmin extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(HalamanUtamaAdmin.this, MinumanAdmin.class);
                 startActivity(intent);
+            }
+        });
+
+        logoutAdmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent logoutAdmin = new Intent(HalamanUtamaAdmin.this, AdminLogin.class);
+                startActivity(logoutAdmin);
             }
         });
 
